989dfe4db62309cd2b3cc41a82a4e09a3d6fc81c
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-11-05 18:01:28 +0200

    Shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20;
+
+    void Start()
+    {
+        Destroy(gameObject,2f);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -5,13 +5,26 @@ public class Tank : MonoBehaviour
     public float speed = 5;
     public float rotateSpeed = 90;
 
+    public string vertical;
+    public string horizontal;
+    public KeyCode shootKey;
+
+    public GameObject bullet;
+    public Transform shootPoint;
+
     void Update()
     {
 
-        var ver = Input.GetAxis("Vertical");
+        var ver = Input.GetAxis(vertical);
         transform.position += transform.forward * speed * ver * Time.deltaTime;
 
-        var hor = Input.GetAxis("Horizontal");
+        var hor = Input.GetAxis(horizontal);
         transform.Rotate(0,rotateSpeed * hor * Time.deltaTime,0);
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            //print("Boom !!!");
+            Instantiate(bullet,shootPoint.position,shootPoint.rotation);
+        }
     }
 }
