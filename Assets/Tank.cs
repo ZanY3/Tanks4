47c0362d45f222485dbb27a5ab131c1a08f78b25
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-11-05 16:59:25 +0200

    Tank Movement

--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Tank : MonoBehaviour
 {
     public float speed = 5;
+    public float rotateSpeed = 90;
 
     void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+
+        var ver = Input.GetAxis("Vertical");
+        transform.position += transform.forward * speed * ver * Time.deltaTime;
+
+        var hor = Input.GetAxis("Horizontal");
+        transform.Rotate(0,rotateSpeed * hor * Time.deltaTime,0);
     }
 }
